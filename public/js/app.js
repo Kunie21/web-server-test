8548772b2e56f3976fbd6c0c0b62ba04fc5f506b
s2-kunie <s2-kunie@bandainamco-ol.co.jp>
2024-05-09 18:38:56 +0900

    update app(random number)

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
-console.log('コンソールログ')
+window.onload = function() {
+    document.getElementById("num1").innerHTML = Math.floor( Math.random() * 100 );
+    document.getElementById("num2").innerHTML = Math.floor( Math.random() * 100 );
+    document.getElementById("num3").innerHTML = Math.floor( Math.random() * 100 );
+};
+
+function sendData() {
+    const form = document.createElement('form');
+    form.method = 'post';
+    form.action = "/api/v1/quiz";
+    document.body.appendChild(form);
+    
+    const formField = document.createElement('input');
+    formField.type = 'hidden';
+    formField.name = "answer";
+    var num = Number(document.getElementById("num1").textContent) + Number(document.getElementById("num2").textContent) + Number(document.getElementById("num3").textContent);
+    var ans = parseInt(document.getElementById("ans").value); // Numberは数値に変換できたらする（7日→NaN）、parseIntは文字列から数値だけ取り出す（7日→7）
+    if (num === ans) {
+        formField.value = 2;
+    } else {
+        formField.value = 1;
+    }
+
+    form.appendChild(formField);
+
+    form.submit();
+}
